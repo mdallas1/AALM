2c0b6c4f97519a80e0b1acc0fc9826a60db4b760
mdallas1 <mdallas256@gmail.com>
2025-02-16 11:11:05 -0600

    added inputParser to aalm and cleaned up

--- aalm.m
+++ aalm.m
@@ -8,83 +8,64 @@ function [out1,out2] = aalm(f,df,x,varargin)
 	-- f and it's Jacobian df are function handles.
 	-- x initial iterate.
 
-	-- optional args {'solver' (char), 'tol' (float), and 'maxiters' (float)}
-	 solvers = {'newton','newt','lm','LM'}.
-
-	-- aalm(f,df,x,'aa') implements Anderson acceleration. 
-	-- aalm(f,df,x,'aa',m) sets depth to m.
-	-- aalm(f,df,x,'gsg',r) implements adaptive gamma safeguarding with tolerance r.
+	-- aalm(f,df,x,'anderson',m) implements Anderson acceleration with depth m.
+	-- aalm(f,df,x,'safeguard',r) implements adaptive gamma safeguarding with tolerance r.
 	-- aalm(f,df,x,'print') prints residual at each iteration. 
+	-- aalm(f,df,x,'tol',tol) sets tolerance. Default tol = 1e-8. 
+	-- aalm(f,df,x,'maxiters',maxiters) sets maximum iterations. Default maxiters = 100.
+
+	-- Plot parameters may be passed with aalm(...,'plot',plot_options), where 
+	plot_options is a cell array. 
 
 	%}
 
-	solvers = {'newton','newt','lm','LM'};
+	assert(isa(f,'function_handle'));
+	assert(isa(df,'function_handle'));
+	assert(isa(x,'double'));
 
-	f = @(x) f(x); df = @(x) df(x);
-	x = x ; fx = f(x); dfx = df(x); 
-	iters = 0; res = norm(fx); I = eye(length(x));
+	p = inputParser(); 
+	p.FunctionName = 'aalm' ; 
 
-	if nargin < 4	
-		tol = 1e-8; maxiters = 100; 
-		solver = 'lm'; aa = 1; m = 1; gsg = 0;
-	end
-	
-	if nargin >= 4 
-		solver_index = find(strcmp('solver',varargin));
-		tol_index = find(strcmp('tol',varargin));
-		maxiters_index = find(strcmp('maxiters',varargin));
-		aa_index = find(strcmp('aa',varargin));
+	default_tol = 1e-8;
+	p.addParameter('tol',default_tol,@isdouble);
 
-		if !isempty(solver_index)
-			solver = varargin{solver_index(1)+1};
-			if isempty(find(strcmp(solver,solvers)))
-				error('Solver not in solver list. Type "help AALM."')
-			end
-		end
+	default_maxiters = 100;
+	p.addParameter('maxiters',default_maxiters,@isdouble);
 
-		if !isempty(tol_index)
-			tol = varargin{tol_index(1)+1};
-		else 
-			tol = 1e-8;
-		end
-	
-		if !isempty(maxiters_index) 
-			maxiters = varargin{maxiters_index(1)+1};
-		else 
-			maxiters = 100;
-		end
-	
-	if !isempty(find(strcmp('aa',varargin)))
-		aa = 1;	
-		aaindex = find(strcmp('aa',varargin))(1);		
-		try 	
-			if isfloat(varargin{aaindex+1})
-				m = varargin{aaindex+1};
-			end
-		catch 
-			disp('!! ERROR: "aa" option requires algorithmic depth.');	
-			return
-		end
+	vld_solver = @(s) any(strcmp(s,{'newton','newt','lm','LM'}));
+	default_solver = 'lm';
+	p.addParameter('solver',default_solver,vld_solver);
+
+	default_depth= 1;
+	p.addParameter('anderson',default_depth,@isfloat);
+
+	default_gsg_tol = 0; 
+	p.addParameter('safeguard',default_gsg_tol,@isfloat);
+
+	default_plot = {}; 
+	p.addParameter('plot',default_plot,@(a) isa(a,'cell'));
+
+	p.addSwitch('print');
+
+	p.parse(varargin{:}); 
+	solver = p.Results.solver; m = p.Results.anderson; gsg_tol = p.Results.safeguard;
+	tol = p.Results.tol; maxiters = p.Results.maxiters; 
+	plot_options = p.Results.plot; 
+
+	if any(strcmp('anderson',varargin));
+		aa = 1;
 	else 
 		aa = 0;
-	end
-
-	if !isempty(find(strcmp('gsg',varargin)))
-		gsg = 1; 
-		gsgindex = find(strcmp('gsg',varargin))(1);		
-		try 	
-			if isfloat(varargin{gsgindex+1})
-				gamma_tol = varargin{gsgindex+1};
-			end
-		catch 
-			disp('!! ERROR: "gsg" option requires safeguarding tolerance.');	
-			return
-		end
+	end 
+	if any(strcmp('safeguard',varargin));
+		gsg = 1;
 	else 
 		gsg = 0;
 	end
 
-	end
+	f = @(x) f(x); df = @(x) df(x);
+	x = x ; fx = f(x); dfx = df(x); 
+	iters = 0; res = norm(fx); I = eye(length(x));
 
 	res_arr = []; x_arr = []; d_arr = []; xaa_arr = [];
 
@@ -102,10 +83,12 @@ function [out1,out2] = aalm(f,df,x,varargin)
 
 			x_arr = [x x_arr]; 
 
-			if strcmp(solver,'lm') || strcmp(solver,'LM')  
+			%if strcmp(solver,'lm') || strcmp(solver,'LM')  
+			if any(strcmp(solver,{'lm','LM'}))
 				mu = 0.5*1e-8*norm(fx)^2; % LM parameter (from KaYaFu03)
 				d = - (dfx'*dfx + mu * I)\(dfx'*fx);
-			elseif strcmp(solver,'newt') || strcmp(solver,'newton')
+			%elseif strcmp(solver,'newt') || strcmp(solver,'newton')
+			elseif any(strcmp(solver,{'newt','newton'}))
 				d = -dfx\fx;
 			end
 			%d_arr = [d d_arr(1:mm-1)];
@@ -114,10 +97,10 @@ function [out1,out2] = aalm(f,df,x,varargin)
 			x = x+d;
 
 		else 
-			if strcmp(solver,'lm') || strcmp(solver,'LM')
+			if any(strcmp(solver,{'lm','LM'}))
 				mu = min(mu,nfx^2);						
 				d = - (dfx'*dfx + mu * I)\(dfx'*fx);
-			elseif strcmp(solver,'newton') || strcmp(solver,'newt')
+			elseif any(strcmp(solver,{'newt','newton'}))
 				d = -dfx\fx;
 			end
 
@@ -126,25 +109,20 @@ function [out1,out2] = aalm(f,df,x,varargin)
 				mm = min(m,iters);
 				x_arr = [x x_arr(:,1:mm)];
 				d_arr = [d d_arr(:,1:mm)];
-				%E = x_arr(:,1:end-1) - x_arr(:,2:end);
-				%F = d_arr(:,1:end-1) - d_arr(:,2:end);
 				E = x_arr(:,1:end-1) - x_arr(:,2:end);
 				F = d_arr(:,1:end-1) - d_arr(:,2:end);
 				gamma = F(:,1:mm)\d_arr(:,1);
 			end
 
 			if gsg 
-				%[sg_gamma,lam,r] = gamma_safgrd(d_arr(:,1),d_arr(:,2),gamma,gamma_tol,1,eye(length(d)));
-				[gamma,lam,r] = gamma_safeguard(d_arr(:,1),d_arr(:,2),gamma,gamma_tol,1,eye(length(d)));
+				[gamma,lam,r] = gamma_safeguard(d_arr(:,1),d_arr(:,2),gamma,gsg_tol,1,eye(length(d)));
 			end
 
 			if aa 
-				 %x = x + d - (E(:,1)+F(:,1))*gamma;
 			     x = x + d - (E(:,1:mm)+F(:,1:mm))*gamma;
 			else 
 				x = x+d;
 			end
-			
 		end
 		iters = iters+1;
 		fx = f(x);
@@ -154,12 +132,13 @@ function [out1,out2] = aalm(f,df,x,varargin)
 		%if iters > 1
 		%	log(res_arr(:,1))/log(res_arr(:,2))
 		%end
-		if !isempty(find(strcmp('print',varargin)))
+		if any(strcmp('print',varargin))
 			fprintf("iter \t res \n-----------------------------------\n%g \t %.3e\n",iters,res);	
 		end
 	end
 	res_arr = [res res_arr];
-	semilogy([0:iters],flip(res_arr),'r-*','Linewidth',1.5);
+	%semilogy([0:iters],flip(res_arr),'r-*','Linewidth',1.5);
+	semilogy([0:iters],flip(res_arr),plot_options{:});
 
 end
 			
